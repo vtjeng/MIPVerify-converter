9becad800cc37ba1ec6d52dccff6576070046fa0
Vincent Tjeng <vincent.tjeng@gmail.com>
2021-04-10 13:19:52 -0700

    Get source data.

---
+++ networks/mnist_sample_1_convert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+This script has been verified to work with 
+
+keras2onnx==1.7.0
+tensorflow==2.3.1
+
+With tensorflow==2.4.1, you will encounter https://github.com/onnx/keras-onnx/issues/651.
+"""
+import os
+
+from tensorflow import keras
+import onnxmltools
+
+SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+model = keras.models.load_model(os.path.join(SCRIPT_DIRECTORY, "mnist_sample_1.h5"))
+onnx_model = onnxmltools.convert_keras(model)
+onnxmltools.utils.save_model(
+    onnx_model, os.path.join(SCRIPT_DIRECTORY, "mnist_sample_1.onnx")
+)
+
